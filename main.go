dc2d3466fc94ac42fb836bf4a48dfef6e9d93dda
victor-s-santos <victor.s.silva@unesp.br>
2020-02-21 10:37:17 -0300

    setting the endpoints methods and path in main.go

--- main.go
+++ main.go
@@ -12,8 +12,13 @@ func heyThere(w http.ResponseWriter, r *http.Request){
 }
 
 func handleRequests(){
+	//Defining the routes
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", heyThere).Methods("GET")
+	myRouter.HandleFunc("/users", Users).Methods("GET")
+	myRouter.HandleFunc("/user/{name}/{email}", NewUser).Methods("POST")
+	myRouter.HandleFunc("/user/{name}", DeleteUser).Methods("DELETE")
+	myRouter.HandleFunc("/user/{name}/{email}", UpdateUser).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 
 }
