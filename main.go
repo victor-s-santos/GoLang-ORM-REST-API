c06f91aef5440e87e85e05d77a61b6e3ee64d1cb
victor-s-santos <victor.s.silva@unesp.br>
2020-02-21 10:17:07 -0300

    api basic system

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func heyThere(w http.ResponseWriter, r *http.Request){
+	fmt.Printf(w, "Hey There")
+}
+
+func handleRequests(){
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", heyThere).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+
+}
+
+func main(){
+	fmt.Println("First GO ORM application")
+
+	handleRequests()
+}
